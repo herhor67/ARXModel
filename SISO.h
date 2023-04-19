8b3a6afc5d092cc3cbbd212a778cb3591c61ec70
Dukat77 <130994054+Dukat77@users.noreply.github.com>
2023-04-19 02:36:37 +0200

    Update SISO.h

--- SISO.h
+++ SISO.h
@@ -3,9 +3,16 @@
 class SISO
 {
 public:
+	// Konstruktor domyślny klasy SISO. Oznaczenie "= default" wskazuje na to,
+	// że konstruktor ten ma zostać wygenerowany automatycznie przez kompilator
 	SISO() = default;
+	// Destruktor wirtualny klasy SISO
 	virtual ~SISO() = default;
 	
+	// Wirtualna metoda iteration reprezentuje jedną iterację systemu SISO.
+	// Metoda ta przyjmuje jako argument sygnał wejściowy i zwraca sygnał wyjściowy,
+	// jest czysto wirtualna, co oznacza, że nie jest ona zdefiniowana 
+	// w tej klasie i musi zostać zaimplementowana w klasie dziedziczącej.
 	virtual double iteration(double) = 0;
 
 };
