76f47172f6d8cf57a665a9be0d01db1c2974f089
Dukat77 <130994054+Dukat77@users.noreply.github.com>
2023-04-19 02:16:14 +0200

    Update ARX.h

--- ARX.h
+++ ARX.h
@@ -1,38 +1,47 @@
 #pragma once
 
-#include "SISO.h"
+#include "SISO.h" // Dołączenie pliku nagłówkowego "SISO.h" do pliku źródłowego
 
 #include <valarray>
 #include <span>
 #include <initializer_list>
 
-
-class ARX : public SISO
+// deklaracja klasy ARX dziedziczącej publicznie po klasie SISO
+class ARX : public SISO 
 {
-	using DS = std::valarray<double>;
+	/* definiuje alias typu danych DS, odnosi się do typu std::valarray<double>, 
+	zamiast używać pełnej nazwy, można użyć skróconej nazwy DS */
+	using DS = std::valarray<double>; 
 
-	DS A; // den
-	DS B; // num
-	unsigned k = 0;
-	double noiseamp = 1;
+	DS A; // den // obiekt klasy valarray przechowujący mianownik
+	DS B; // num  // obiekt klasy valarray przechowujący licznik
+	unsigned k = 0;  // wartość typu unsigned reprezentująca opóźnienie, domyślnie 0
+	double noiseamp = 1; // wartość typu double reprezentująca amplitudę szumu, domyślnie o wartości 1
 
-	DS inBuf;
-	DS outBuf;
+	DS inBuf; // wektor przechowujący wartości wejściowe wraz z opóźnieniem
+	DS outBuf; // wektor przechowujący wartości wyjściowe wraz z opóźnieniem
 
 
 public:
+	/* Konstruktor klasy ARX z argumentami:
+	- A - wektor z danymi typu double reprezentujący mianownik
+	- B - wektor z danymi typu double reprezentujący licznik
+	- dly - wartość typu unsigned reprezentująca opóźnienie, domyślnie 0
+	- ns - wartość typu double reprezentująca amplitudę szumu, domyślnie 1 */
 	ARX(std::initializer_list<double>, std::initializer_list<double>, unsigned = 0, double = 0);
-	~ARX();
+	~ARX(); // deklaracja destruktora klasy ARX
 
-	double iteration(double);
+	double iteration(double); // deklaracja funkcji wykonującej jeden krok symulacji
 
-	void setNum(std::initializer_list<double>);
+	/* deklaracja funkcji ustawiającej wartości wektora B oraz przesuwającej 
+	wartości wektora inBuf o odpowiednią ilość elementów */
+	void setNum(std::initializer_list<double>); 
 
-	void setDen(std::initializer_list<double>);
+	void setDen(std::initializer_list<double>); // deklaracja funkcji ustawiającej wartości wektora A
 
-	void setup();
+	void setup(); // deklaracja funkcji pomocniczej, której można użyć do inicjalizacji różnych wartości
 
-	static double getNoise();
+	static double getNoise(); // deklaracja funkcji statycznej, która może generować szum
 
 };
 
