8c27bdf666da27c0f8eeea22a5ced5c322255c05
Dukat77 <130994054+Dukat77@users.noreply.github.com>
2023-04-20 17:39:43 +0200

    Update Simulation.h

--- Simulation.h
+++ Simulation.h
@@ -5,17 +5,26 @@
 
 #include <string>
 
+/* Deklaracja klasy Simulation, która reprezentuje symulację w programie.
+ Klasa składa się z dwóch obiektów klasy ARX i PID, które są częściami symulowanego systemu */
 class Simulation
 {
 public:
 
-	ARX arx;
-	PID pid;
+	ARX arx; // obiekt klasy ARX reprezentujący model matematyczny systemu regulacji
+	PID pid; // obiekt klasy PID reprezentujący regulator PID systemu regulacji
 
+	// Konstruktor klasy Simulation, który inicjuje obiekty ARX i PID 
 	Simulation(const ARX&, const PID&);
+	
+	// Konstruktor domyślny klasy Simulation, który nie przyjmuje żadnych argumentów.
 	Simulation();
+	
+	// Destruktor klasy Simulation, który jest domyślny.
 	~Simulation() = default;
 
+	/* Metoda save zapisuje symulację do pliku. 
+	Wykorzystywana jest biblioteka json.hpp, która umożliwia zapisywanie danych w formacie JSON. */
 	void save(std::string);
 };
 
