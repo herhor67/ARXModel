021284f3f2fd71c8cfa8bcb927c8b1dbf3a2455f
Dukat77 <130994054+Dukat77@users.noreply.github.com>
2023-04-20 17:31:15 +0200

    Update Simulation.cpp

--- Simulation.cpp
+++ Simulation.cpp
@@ -4,21 +4,29 @@
 #include <iostream>
 #include <fstream>
 
+// Dołączenie biblioteki json.hpp i nadanie jej aliasu json
 #include "json.hpp"
 using json = nlohmann::json;
 
+// Definicja konstruktora z parametrami klasy Simulation, przyjmującego obiekty klasy ARX i PID
 Simulation::Simulation(const ARX& a, const PID& p) : arx(a), pid(p) {}
 
+// Konstruktor domyślny klasy Simulation
 Simulation::Simulation() {}
 
-
+// Metoda save klasy Simulation, zapisująca parametry symulacji do pliku JSON o nadanej nazwie
 void Simulation::save(std::string file)
 {
-	json j;
+	json j; // Tworzenie obiektu json
+	
+	// Zapisanie parametrów obiektów ARX i PID do obiektu json
 	j["ARX"] = arx;
 	j["PID"] = pid;
 
+	// Następuje otwarcie pliku i zapisuje się obiektu json
 	std::ofstream out(file);
+	
+	// Zapisanie danych w formacie JSON do pliku za pomocą strumienia wyjściowego
 	out << j;
 
 }
