c4ff00433b6e5c22acd5277337adc7407f16061a
Dukat77 <130994054+Dukat77@users.noreply.github.com>
2023-05-16 01:47:53 +0200

    Update Simulation.h

--- Simulation.h
+++ Simulation.h
@@ -16,6 +16,8 @@ public:
 
 	// Konstruktor klasy Simulation, który inicjuje obiekty ARX i PID 
 	Simulation(const ARX&, const PID&);
+	
+	// Konstruktor klasy Simulation, który inicjuje obiekt ARX na podstawie ścieżki do pliku konfiguracyjnego.
 	Simulation(const std::string&);
 	
 	// Konstruktor domyślny klasy Simulation, który nie przyjmuje żadnych argumentów.
@@ -24,13 +26,10 @@ public:
 	// Destruktor klasy Simulation, który jest domyślny.
 	~Simulation() = default;
 
-
+	// Metoda run wykonuje symulację przez określoną liczbę iteracji dla podanego wejścia.
 	void run(size_t, double = 0);
 
 	/* Metoda save zapisuje symulację do pliku. 
 	Wykorzystywana jest biblioteka json.hpp, która umożliwia zapisywanie danych w formacie JSON. */
 	void save(const std::string&);
 };
-
-
-
