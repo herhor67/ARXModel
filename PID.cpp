1fb3d45fdbf399150b4f9ce17e5b9b3406913275
Dukat77 <130994054+Dukat77@users.noreply.github.com>
2023-05-16 01:44:16 +0200

    Update PID.cpp

--- PID.cpp
+++ PID.cpp
@@ -1,6 +1,5 @@
 ﻿#include "PID.h"
 
-
 // Konstruktor klasy PID przyjmuje opcjonalne wartości składowych P, I i D. Domyślnie ustawione na 0.
 PID::PID(double p, double i, double d) : P(p), I(i), D(d) {}
 
@@ -10,9 +9,9 @@ PID::~PID() = default; // Destruktor klasy PID jest zdefiniowany domyślnie.
 Przyjmuje jako argument sygnał wejściowy (błąd regulacji) i zwraca sygnał wyjściowy (sygnał sterujący) */
 double PID::sim(double err)
 {
-	sumerr += err;
-	double differr = err - lasterr;
-	lasterr = err;
+	sumerr += err; // Akumulacja błędu regulacji
+	double differr = err - lasterr; // Obliczenie różnicy błędu regulacji
+	lasterr = err; // Przypisanie bieżącego błędu regulacji jako poprzedni błąd
 
-	return P * err + I * sumerr + D * differr;
+	return P * err + I * sumerr + D * differr; // Obliczenie sygnału sterującego z wykorzystaniem składowych P, I i D
 }
