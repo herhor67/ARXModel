4ed9b54c8b66996261e0e36a8e3db6691612be0f
Dukat77 <130994054+Dukat77@users.noreply.github.com>
2023-05-16 01:45:16 +0200

    Update settings.h

--- settings.h
+++ settings.h
@@ -1,9 +1,14 @@
 #pragma once
 
-#include <cstddef>
+// Wczytanie pliku nagłówkowego "cstddef", który zawiera 
+// definicje typów i stałych związanych z rozmiarem i indeksowaniem.
+#include <cstddef> 
 
-using std::size_t;
+using std::size_t; // Użycie typu size_t.
 
 #include <iostream>
-using std::cout;
+
+// Użycie obiektu cout do wypisywania danych na standardowym wyjściu.
+using std::cout; 
+// Użycie obiektu endl do wstawiania znaku nowej linii w strumieniu wyjścia.
 using std::endl;
