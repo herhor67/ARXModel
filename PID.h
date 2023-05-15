655004a1974d8d6dfb890555a63eaaa51d5c949e
Dukat77 <130994054+Dukat77@users.noreply.github.com>
2023-05-16 01:42:26 +0200

    Update PID.h

--- PID.h
+++ PID.h
@@ -2,7 +2,7 @@
 
 #include "SISO.h"
 
-#include "json.hpp"
+#include "json.hpp" // Wczytanie biblioteki "json.hpp", która umożliwia serializację i deserializację obiektów JSON.
 
 // Klasa PID dziedziczy po klasie SISO i definiuje regulator PID
 class PID : public SISO
@@ -11,14 +11,16 @@ class PID : public SISO
 	friend class Simulation;
 public:
 	// Składowe klasy PID
-	double P, I, D;
-	double sumerr, lasterr;
+	double P, I, D; // Współczynniki P, I, D dla regulatora PID.
+	double sumerr, lasterr; // Suma błędów i ostatni błąd.
 
 	// Konstruktor klasy PID przyjmuje opcjonalne wartości składowych P, I i D. Domyślnie ustawione na 0.
 	PID(double = 0, double = 0, double = 0);
 	
+	// Destruktor klasy PID
 	~PID();
-
+	
+	// Funkcja sim() symuluje działanie regulatora PID dla określonej wartości
 	double sim(double);
 	
 	// Szablon generujący serializację i deserializację klasy PID za pomocą biblioteki nlohmann::json.
