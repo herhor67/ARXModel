f4b314e4f2121f5787d8e5f5fda6a59da933e3ce
Dukat77 <130994054+Dukat77@users.noreply.github.com>
2023-05-22 02:39:30 +0200

    Update Simulation.h

--- Simulation.h
+++ Simulation.h
@@ -6,32 +6,37 @@
 
 #include <string>
 
-/* Deklaracja klasy Simulation, która reprezentuje symulację w programie.
- Klasa składa się z dwóch obiektów klasy ARX i PID, które są częściami symulowanego systemu */
+/// \class Simulation
+/// \brief Klasa reprezentująca symulację systemu regulacji.
+///
+/// Klasa Simulation składa się z dwóch obiektów: ARX i PID.
 class Simulation
 {
 public:
 
-	ARX arx; // obiekt klasy ARX reprezentujący model matematyczny systemu regulacji
-	PID pid; // obiekt klasy PID reprezentujący regulator PID systemu regulacji
-	Generator gen;
+	ARX arx; ///< Obiekt klasy ARX reprezentujący model matematyczny systemu regulacji.
+	PID pid; ///< Obiekt klasy PID reprezentujący regulator PID systemu regulacji.
+	Generator gen; ///< Obiekt klasy Generator odpowiedzialny za generowanie danych wejściowych.
 
-	// Konstruktor klasy Simulation, który inicjuje obiekty ARX i PID 
+	/// \brief Konstruktor klasy Simulation.
+	/// \param arx Obiekt klasy ARX, model matematyczny systemu.
+    /// \param pid Obiekt klasy PID, regulator PID systemu.
+    /// \param gen Obiekt klasy Generator, generator danych wejściowych. 
 	Simulation(ARX&&, PID&&, Generator&&);
 	
-	// Konstruktor klasy Simulation, który inicjuje obiekty na podstawie ścieżki do pliku konfiguracyjnego.
+	/// \brief Konstruktor klasy Simulation.
 	Simulation(const std::string&);
 	
-	// Konstruktor domyślny klasy Simulation, który nie przyjmuje żadnych argumentów.
+	/// \brief Konstruktor domyślny klasy Simulation.
 	Simulation();
 	
-	// Destruktor klasy Simulation, który jest domyślny.
+	/// \brief Destruktor klasy Simulation.
 	~Simulation() = default;
 
-	// Metoda run wykonuje symulację przez określoną liczbę iteracji dla podanego wejścia.
+	/// \brief Metoda run wykonuje symulację przez określoną liczbę iteracji dla podanego wejścia.
 	void run(size_t);
 
-	/* Metoda save zapisuje symulację do pliku. 
-	Wykorzystywana jest biblioteka json.hpp, która umożliwia zapisywanie danych w formacie JSON. */
+	/// \brief Metoda save zapisuje symulację do pliku w formacie JSON. 
+	///Wykorzystywana jest biblioteka json.hpp, która umożliwia zapisywanie danych w formacie JSON.
 	void save(const std::string&);
 };
