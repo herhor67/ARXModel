d622f70b492708d5cc62e3df28c675f69b0210d7
Dukat77 <130994054+Dukat77@users.noreply.github.com>
2023-05-22 02:25:44 +0200

    Update helpers.h

--- helpers.h
+++ helpers.h
@@ -2,17 +2,21 @@
 
 #include "settings.h" 
 
-#include <valarray> // Wczytanie biblioteki "valarray", która zawiera definicje i funkcje do obsługi tablic wartości typu T
+#include <valarray> ///< Wczytanie biblioteki "valarray", która zawiera definicje i funkcje do obsługi tablic wartości typu T
 
-#include <iostream> // Wczytanie biblioteki "iostream", która zawiera definicje i funkcje do obsługi strumieni wejścia/wyjścia
+#include <iostream> ///< Wczytanie biblioteki "iostream", która zawiera definicje i funkcje do obsługi strumieni wejścia/wyjścia
 
-// Zdefiniowanie szablonu funkcji dla typu T.
+/// Zdefiniowanie szablonu funkcji dla typu T.
+/// \tparam T Typ danych.
+/// \param o Strumień wyjściowy.
+/// \param va Tablica wartości typu T.
+/// \return Referencja do strumienia wyjściowego.
 template <typename T>
 std::ostream& operator<<(std::ostream& o, const std::valarray<T>& va)
 {
 	o << '(';
-	for (const T& v : va) // Iteracja po wszystkich elementach tablicy va
+	for (const T& v : va) ///< Iteracja po wszystkich elementach tablicy va
 		o << v << ',';
 	o << ')'; 
-	return o; // Zwrócenie referencji do strumienia wyjścia o 
+	return o; 
 }
