65146b3c423643a7fdd3c2a862fc9a64540463aa
Dukat77 <130994054+Dukat77@users.noreply.github.com>
2023-06-04 13:16:35 +0200

    Update Signal.cpp

--- Signal.cpp
+++ Signal.cpp
@@ -1,20 +1,47 @@
 #include "Signal.h"
 
-
+/**
+ * @brief Konstruktor klasy SignalHdl.
+ *
+ * Konstruktor tworzy obiekt SignalHdl przyjmując wskaźnik przenoszony na obiekt sygnału.
+ * @param p Wskaźnik przenoszony na obiekt sygnału.
+ */
 SignalHdl::SignalHdl(SignalPtr&& p) : ptr(std::move(p)) {}
 
+/**
+ * @brief Operator dereferencji.
+ *
+ * Operator dereferencji (*) zwraca referencję do obiektu sygnału przechowywanego przez SignalHdl.
+ * @return Referencja do obiektu sygnału.
+ */
 Signal& SignalHdl::operator*() const
 {
 	return *ptr.get();
 }
+
+/**
+ * @brief Operator dostępu do składowych.
+ *
+ * Operator dostępu do składowych (->) zwraca wskaźnik do obiektu sygnału przechowywanego przez SignalHdl.
+ * @return Wskaźnik do obiektu sygnału.
+ */
 Signal* SignalHdl::operator->() const
 {
 	return ptr.get();
 }
 
-
+/**
+ * @brief Funkcja to_json.
+ *
+ * Funkcja to_json służy do serializacji obiektu SignalHdl do formatu JSON.
+ * Konwertuje obiekt SignalHdl na obiekt JSON, zapisując typ sygnału i jego parametry.
+ *
+ * @param j Obiekt JSON, do którego zostanie zapisana serializacja.
+ * @param o Obiekt SignalHdl, który ma zostać zserializowany.
+ */
 void to_json(json& j, const SignalHdl& o)
 {
+	// Pobranie typu sygnału przechowywanego przez SignalHdl
 	SignalType t = o.ptr->type();
 	j["t"] = static_cast<SignalEnumT>(t);
 
@@ -50,8 +77,18 @@ void to_json(json& j, const SignalHdl& o)
 	j["p"] = obj;
 }
 
+/**
+ * @brief Funkcja from_json.
+ *
+ * Funkcja from_json służy do deserializacji obiektu SignalHdl z formatu JSON.
+ * Konwertuje obiekt JSON na obiekt SignalHdl, odtwarzając typ sygnału i jego parametry.
+ *
+ * @param j Obiekt JSON, który ma zostać zdeserializowany.
+ * @param o Obiekt SignalHdl, do którego zostanie zapisana deserializacja.
+ */
 void from_json(const json& j, SignalHdl& o)
 {
+	// Pobranie typu sygnału z obiektu JSON
 	SignalType t = static_cast<SignalType>(j["t"]);
 
 	switch (t)
@@ -82,4 +119,3 @@ void from_json(const json& j, SignalHdl& o)
 		break;
 	}
 }
-
