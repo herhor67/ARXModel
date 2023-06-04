1ef778ed2e19a55f08d3d015df2a95af8515d24e
Dukat77 <130994054+Dukat77@users.noreply.github.com>
2023-06-04 13:33:32 +0200

    Update Simulation.h

--- Simulation.h
+++ Simulation.h
@@ -17,15 +17,17 @@ public:
 	ARX arx; ///< Obiekt klasy ARX reprezentujący model matematyczny systemu regulacji.
 	PID pid; ///< Obiekt klasy PID reprezentujący regulator PID systemu regulacji.
 	Generator gen; ///< Obiekt klasy Generator odpowiedzialny za generowanie danych wejściowych.
-	size_t len;
+	size_t len; ///< Długość symulacji.
 
 	/// \brief Konstruktor klasy Simulation.
 	/// \param arx Obiekt klasy ARX, model matematyczny systemu.
     /// \param pid Obiekt klasy PID, regulator PID systemu.
-    /// \param gen Obiekt klasy Generator, generator danych wejściowych. 
+    /// \param gen Obiekt klasy Generator, generator danych wejściowych.
+	/// \param len Długość symulacji.
 	Simulation(ARX&&, PID&&, Generator&&, size_t = 0);
 	
 	/// \brief Konstruktor klasy Simulation.
+	/// \param filename Nazwa pliku z danymi konfiguracyjnymi.
 	Simulation(const std::string&);
 	
 	/// \brief Konstruktor domyślny klasy Simulation.
@@ -35,9 +37,11 @@ public:
 	~Simulation() = default;
 
 	/// \brief Metoda run wykonuje symulację przez określoną liczbę iteracji dla podanego wejścia.
+	/// \param inputFilename Nazwa pliku z danymi wejściowymi.
 	void run(const std::string& = "");
 
 	/// \brief Metoda save zapisuje symulację do pliku w formacie JSON. 
-	///Wykorzystywana jest biblioteka json.hpp, która umożliwia zapisywanie danych w formacie JSON.
+	/// Wykorzystywana jest biblioteka json.hpp, która umożliwia zapisywanie danych w formacie JSON.
+	/// \param outputFilename Nazwa pliku wyjściowego.
 	void save(const std::string&);
 };
